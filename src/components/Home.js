9c1d5e0cde70c7d5805e6f8a5363041a2f85e894
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-08-02 14:28:19 -0700

    Still making Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,19 +6,29 @@ const Home = () => {
         Welcome to Your Game Lister! Follow the instructions to add or delete
         your game!
       </h3>
-      <h2>Click on Add Game in the Navigation Bar</h2>
+      <h2>Click on Add Game in the Navigation Bar.</h2>
       <h2>
         If your gaming platform is not included in the dropdown, click on Add
-        Platform and enter the name of your platform
+        Platform and enter the name of your platform.
       </h2>
       <h2>
         Once a platform is added, it will redirect you to the Add Game form
-        where the platform will now be available in the drop down
+        where the platform will now be available in the drop down.
       </h2>
       <h2>
         Once game the game info is entered and you click the "Add Game" button,
         you will be redirected to your display list of added games.
       </h2>
+      <h2>
+        While in your Display List page there you can set the game to either
+        "Not Playing" or "Currently Playing".
+      </h2>
+      <h2>
+        If any game ceases to be in your collection, simply click "Delete Game"
+        and Poof! GONE!
+      </h2>
+      <br />
+      <h2>Last but not least, HAVE FUN!</h2>
     </div>
   );
 };
